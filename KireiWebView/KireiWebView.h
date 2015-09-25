e388cfa55f948a6c7d01c01098da03b1858ed2c1
Takuya.Okamoto <blackn.red42@gmail.com>
2015-09-25 18:52:00 +0900

    Initial Commit

---
+++ KireiWebView/KireiWebView.h
@@ -0,0 +1,19 @@
+//
+//  KireiWebView.h
+//  KireiWebView
+//
+//  Created by Takuya Okamoto on 2015/09/25.
+//  Copyright © 2015年 Uniface. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KireiWebView.
+FOUNDATION_EXPORT double KireiWebViewVersionNumber;
+
+//! Project version string for KireiWebView.
+FOUNDATION_EXPORT const unsigned char KireiWebViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KireiWebView/PublicHeader.h>
+
+
