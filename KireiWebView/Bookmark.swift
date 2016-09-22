e110f6eb6aa49314eec7eb2cb00e8c08571af6fa
Takuya Okamoto <blackn.red42@gmail.com>
2016-09-23 07:01:08 +0900

    small fix

--- KireiWebView/Bookmark.swift
+++ KireiWebView/Bookmark.swift
@@ -39,7 +39,6 @@ class BookmarkStore {
         return [
             Bookmark(url: "https://google.com", title: "google"),
             Bookmark(url: "http://raw.senmanga.com/release", title: "sen manga"),
-            Bookmark(url: "http://urasunday.com/mobupsycho100/", title: "mobupsycho100")
         ]
     }
     
